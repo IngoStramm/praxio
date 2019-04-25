c8a2c2d3a97f3f456de82e4daf15e7a7170922c8
marcioseabra <mscriacoes@gmail.com>
2019-04-25 03:10:54 -0300

    Header menu - Toogle Update
    Adicionado a opção TOOGLE no menu + FIX do Header no mobile.

---
+++ assets/js/menu-popover.js
@@ -0,0 +1,11 @@
+jQuery( function( $ ) {
+    $( document ).ready( function() {
+        $('.box-vendas a').attr('href', 'javascript:;'); //Elimino o # no atributo href
+        $('.box-vendas a').attr('data-trigger', 'focus'); //faço funcionar apenas em foco
+        $('.box-clientes a').attr('href', 'javascript:;'); //Elimino o # no atributo href
+        $('.box-clientes a').attr('data-trigger', 'focus'); //faço funcionar apenas em foco
+
+        $('.box-vendas a').popover({content: '<div class="title">Ligue para</div><div class="fone">0800 00 00 100</div><div class="action">ou <a href="#">INICIE O CHAT</a></div>', html: true, placement: "bottom"});
+        $('.box-clientes a').popover({content: '<div class="title">Suporte</div><div class="fone">5018-2525</div><div class="subtitle">ou acesse:</div><div class="popover-url"><a href="#">Portal do Cliente</a></div>', html: true, placement: "bottom"});
+    });
+});
