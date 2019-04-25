977144ce96586338d9340320c57516546e111d96
Ingo Stramm <ingostramm@gmail.com>
2019-04-25 15:54:06 -0300

    Correção do link no menu

--- assets/js/menu-popover.js
+++ assets/js/menu-popover.js
@@ -6,6 +6,6 @@ jQuery( function( $ ) {
         $('.box-clientes a').attr('data-trigger', 'focus'); //faço funcionar apenas em foco
 
         $('.box-vendas a').popover({content: '<div class="title">Ligue para</div><div class="fone">0800 00 00 100</div><div class="action">ou <a href="#">INICIE O CHAT</a></div>', html: true, placement: "bottom"});
-        $('.box-clientes a').popover({content: '<div class="title">Suporte</div><div class="fone">5018-2525</div><div class="subtitle">ou acesse:</div><div class="popover-url"><a href="http://portaldocliente.praxio.com.br/" target="_blank">Portal do Cliente</a></div>', html: true, placement: "bottom"});
+        $('.box-clientes a').popover({content: '<div class="title">Suporte</div><div class="fone">5018-2525</div><div class="subtitle">ou acesse:</div><div class="popover-url"><a href="http://praxio.desenvolvefacil.com/portal-do-cliente/" target="_blank">Conexão Praxio</a></div>', html: true, placement: "bottom"});
     });
 });
