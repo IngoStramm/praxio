cb87b198f491b57c9d1a5a7ab8c54f48d8f4df2a
Ingo Stramm <ingostramm@gmail.com>
2019-05-10 15:07:47 -0300

    Estilo do Modelo de Página Padrão

--- content-page.php
+++ content-page.php
@@ -8,27 +8,28 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<div class="container">
-		<div class="row div col-md-12">
-			<?php the_title( '<header class="entry-header"><h1 class="entry-title">', '</h1></header><!-- .entry-header -->' ); ?>
+	<div class="page-banner" style="background-image: url(<?php echo get_stylesheet_directory_uri(); ?>/assets/images/page-banner.jpg);">
+		
+		<div class="container">
+			<div class="row div col-md-12">
+				<div class="clearfix p-t-150"></div>
+				<h3 class="page-banner-titulo"><?php echo get_the_title(); ?></h3>
+				<div class="clearfix p-b-150"></div>
+			</div>
+			<!-- /.row div col-md-12 -->
 		</div>
-		<!-- /.row div col-md-12 -->
+		<!-- /.container -->
+
 	</div>
-	<!-- /.container -->
+	<!-- /.page-banner -->
+
+	<div class="clearfix m-t-80"></div>
 
 	<div class="container">
 		<div class="row">
 			<div class="col-md-12 b40-1">
-				<div class="entry-content">
-					<?php
-						the_content();
-						wp_link_pages( array(
-							'before'      => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'odin' ) . '</span>',
-							'after'       => '</div>',
-							'link_before' => '<span>',
-							'link_after'  => '</span>',
-						) );
-					?>
+				<div class="entry-content page-content">
+					<?php the_content(); ?>
 				</div><!-- .entry-content -->
 			</div>
 			<!-- /.col-md-12 -->
@@ -37,3 +38,5 @@
 	</div>
 	<!-- /.container -->
 </article><!-- #post-## -->
+
+<div class="clearfix m-b-80"></div>
