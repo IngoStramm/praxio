90407d13e68333c3bc3c870388231e75459bd8bc
Ingo Stramm <ingostramm@gmail.com>
2019-05-29 16:05:01 -0300

    Seção Integração (Home)

--- content-front-page.php
+++ content-front-page.php
@@ -396,6 +396,133 @@
 	<div class="clearfix m-t-140"></div>
 
 	<?php
+		$show_extra_section_1 = get_post_meta( $post_id, 'front_page_extra_section_1_show', true );
+		$front_page_extra_section_1_title = get_post_meta( $post_id, 'front_page_extra_section_1_title', true );
+		$front_page_extra_section_1_title_featured = get_post_meta( $post_id, 'front_page_extra_section_1_title_featured', true );
+		// Logos
+		$front_page_extra_section_1_logos_items = get_post_meta( $post_id, 'front_page_extra_section_1_logos_item', true );
+		// Sanfona
+		$front_page_extra_section_1_sanfona_items = get_post_meta( $post_id, 'front_page_extra_section_1_sanfona_item', true );
+	?>
+
+	<?php if( $show_extra_section_1 ) : ?>
+
+		<div class="extra-section-1 ov-h">
+			
+			<div class="container">
+
+				<div class="row">
+					
+					<div class="col-md-12">
+						
+						<h3 class="extra-section-1-title text-center" style="position: relative; z-index: 10;"><?php echo $front_page_extra_section_1_title; ?></h3>
+
+						<h1 class="extra-section-1-title-featured text-center" style="position: relative;">
+
+							<span style="position: relative; background-color: #fff; z-index: 9; padding: 0 20px"><?php echo $front_page_extra_section_1_title_featured; ?></span>
+
+							<div class="prx-linha prx-linha-b" style="position: absolute; bottom: 50%; left: 0; right: 0;">
+								
+								<div class="prx-linha prx-linha-t prx-linha-l prx-linha-radius-1 hidden-xs hidden-sm" style="position: absolute; top: 108px; right: 100%; width: 140px; height: 500px;"></div>
+
+								<div class="prx-linha prx-linha-t prx-linha-r prx-linha-radius-2 hidden-xs hidden-sm" style="position: absolute; top: 108px; left: 100%; width: 140px; height: 500px;"></div>
+
+							</div>
+						</h1>
+
+					</div>
+					<!-- /.col-md-12 -->
+
+					<div class="col-md-12">
+
+						<ul class="extra-section-1-logos">
+						
+							<?php foreach( $front_page_extra_section_1_logos_items as $item ) : ?>
+
+								<?php //$utils->debug( $item ); ?>
+
+								<li class="extra-section-1-logos-item">
+									<div class="extra-section-1-logos-item-img" style="background-image: url(<?php echo $item[ 'front_page_extra_section_1_logos_img' ]; ?>);">
+									</div>
+								</li>
+
+							<?php endforeach; ?>
+
+						</ul>
+						<!-- /.extra-section-1-logos -->
+
+					</div>
+					<!-- /.col-md-12 -->
+
+					<div class="clearfix m-b-40"></div>
+
+					<div class="col-md-12">
+
+						<?php 
+							$rows = ceil( count( $front_page_extra_section_1_sanfona_items ) / 3 );
+							$lists = array_chunk( $front_page_extra_section_1_sanfona_items, $rows );
+						?>
+
+						<div class="row">
+							
+							<?php foreach ( $lists as $k => $columns ) : ?>
+
+								<div class="col-md-4">
+								
+									<?php foreach ( $columns as $id => $column ) : ?>
+
+										<?php if( !empty( $column ) && !is_null( $column ) ) : ?>
+
+											<?php $titulo = $column[ 'front_page_extra_section_1_sanfona_title' ] ?>
+											<?php $marcas = $column[ 'front_page_extra_section_1_sanfona_brand' ] ?>
+
+											<button data-toggle="collapse" data-target="#collapse-<?php echo $k . '-' . $id; ?>" class="extra-section-1-logos-collapse-btn collapsed">
+												<span><?php echo $titulo; ?></span>
+												<i class="fa fa-angle-up"></i>
+												<i class="fa fa-angle-down"></i>
+											</button>
+											<div class="clearfix"></div>
+
+											<ul id="collapse-<?php echo $k . '-' . $id; ?>" class="collapse extra-section-1-logos-collapse-content">
+
+												<?php foreach( $marcas as $key => $marca ): ?>
+													
+													<li class="extra-section-1-logos-collapse-content-item"><?php echo $marca; ?></li>
+
+												<?php endforeach ?>
+												
+											</ul>
+
+											<div class="clearfix m-b-10"></div>
+
+										<?php endif; ?>
+									
+									<?php endforeach; ?>
+
+								</div>
+								<!-- /.col-md-4 -->
+
+							<?php endforeach; ?>
+
+						</div>
+						<!-- /.row -->
+
+					</div>
+					<!-- /.col-md-12 -->
+
+				</div>
+				<!-- /.row -->
+
+			</div>
+			<!-- /.container -->
+
+		</div>
+		<!-- /.extra-section-1 -->
+
+		<div class="clearfix m-b-80"></div>
+	<?php endif; ?>
+
+	<?php
 		$show_section_4 = get_post_meta( $post_id, 'front_page_section_4_show', true );
 		$front_page_section_4_title = get_post_meta( $post_id, 'front_page_section_4_title', true );
 		$front_page_section_4_text_1_linha_2 = get_post_meta( $post_id, 'front_page_section_4_text_1_linha_2', true );
--- inc/laf/metaboxes/front-page.php
+++ inc/laf/metaboxes/front-page.php
@@ -339,6 +339,172 @@ function front_page_register_metabox() {
 		)
 	) );
 
+	$prefix = 'front_page_extra_section_1_';
+
+	/**
+	 * Sample metabox to demonstrate each field type included
+	 */
+	$cmb_extra_section_1 = new_cmb2_box( array(
+		'id'            => $prefix . 'metabox',
+		'title'         => esc_html__( 'Seção Extra #1', 'cmb2' ),
+		'object_types'  => array( 'page' ), // Post type
+		'show_on_cb' => 'laf_show_if_front_page', // function should return a bool value
+	) );
+
+	$cmb_extra_section_1->add_field( array(
+		'name' => esc_html__( 'Exibir Seção Extra #1?', 'cmb2' ),
+		'id'   => $prefix . 'show',
+		'type' => 'checkbox',
+	) );
+
+	$cmb_extra_section_1->add_field( array(
+		'name'       => esc_html__( 'Título', 'cmb2' ),
+		'id'         => $prefix . 'title',
+		'type'       => 'text',
+		'sanitization_cb' => 'prx_allow_html',
+	) );
+
+	$cmb_extra_section_1->add_field( array(
+		'name'       => esc_html__( 'Título Destaque', 'cmb2' ),
+		'id'         => $prefix . 'title_featured',
+		'type'       => 'text',
+	) );
+
+	$prefix = 'front_page_extra_section_1_logos_';
+
+	$cmb_extra_section_1_logos = new_cmb2_box( array(
+		'id'            => $prefix . 'items',
+		'title'         => __( 'Logos da Seção Extra #1', 'laf' ),
+		'object_types'  => array( 'page' ), // Post type
+		'show_on_cb' => 'laf_show_if_front_page', // function should return a bool value
+	) );
+
+	$group_field_id = $cmb_extra_section_1_logos->add_field( array(
+		'id'          => $prefix . 'item',
+		'type'        => 'group',
+		'description' => esc_html__( 'Adicione os logos da Seção Extra #1.', 'cmb2' ),
+		'options'     => array(
+			'group_title'   => esc_html__( 'Logo {#}', 'cmb2' ), // {#} gets replaced by row number
+			'add_button'    => esc_html__( 'Adicionar', 'cmb2' ),
+			'remove_button' => esc_html__( 'Remover', 'cmb2' ),
+			'sortable'      => true, // beta
+			// 'closed'     => true, // true to have the groups closed by default
+		),
+	) );
+	
+	$cmb_extra_section_1_logos->add_group_field( $group_field_id, array(
+		'name' 				=> __( 'Arquivo', 'laf' ),
+		'id'   				=> $prefix . 'img',
+		'type' 				=> 'file',
+		'text'				=> array(
+			'add_upload_file_text' => __('Adicionar arquivo', 'laf') // Change upload button text. Default: "Add or Upload File"
+		),
+		'attributes' 		=> array(
+			'placeholder' 	=> 'http://',
+			// 'required'		=> ''
+		)
+	) );
+
+	$prefix = 'front_page_extra_section_1_sanfona_';
+
+	$cmb_extra_section_1_sanfona = new_cmb2_box( array(
+		'id'            => $prefix . 'items',
+		'title'         => __( 'Itens da Seção Extra #1', 'laf' ),
+		'object_types'  => array( 'page' ), // Post type
+		'show_on_cb' => 'laf_show_if_front_page', // function should return a bool value
+	) );
+
+	$group_field_id = $cmb_extra_section_1_sanfona->add_field( array(
+		'id'          => $prefix . 'item',
+		'type'        => 'group',
+		'description' => esc_html__( 'Adicione o item da Seção Extra #1.', 'cmb2' ),
+		'options'     => array(
+			'group_title'   => esc_html__( 'Item {#}', 'cmb2' ), // {#} gets replaced by row number
+			'add_button'    => esc_html__( 'Adicionar', 'cmb2' ),
+			'remove_button' => esc_html__( 'Remover', 'cmb2' ),
+			'sortable'      => true, // beta
+			// 'closed'     => true, // true to have the groups closed by default
+		),
+	) );
+	
+	$cmb_extra_section_1_sanfona->add_group_field( $group_field_id, array(
+		'name' 				=> __( 'Título', 'laf' ),
+		'id'   				=> $prefix . 'title',
+		'type' 				=> 'text',
+	) );
+
+	$cmb_extra_section_1_sanfona->add_group_field( $group_field_id, array(
+		'name' 				=> __( 'Marca', 'laf' ),
+		'id'   				=> $prefix . 'brand',
+		'type' 				=> 'text',
+		'repeatable'		=> true,
+	) );
+
+	$prefix = 'front_page_extra_section_2_';
+
+	/**
+	 * Sample metabox to demonstrate each field type included
+	 */
+	$cmb_extra_section_2 = new_cmb2_box( array(
+		'id'            => $prefix . 'metabox',
+		'title'         => esc_html__( 'Seção Extra #2', 'cmb2' ),
+		'object_types'  => array( 'page' ), // Post type
+		'show_on_cb' => 'laf_show_if_front_page', // function should return a bool value
+	) );
+
+	$cmb_extra_section_2->add_field( array(
+		'name' => esc_html__( 'Exibir Seção Extra #2?', 'cmb2' ),
+		'id'   => $prefix . 'show',
+		'type' => 'checkbox',
+	) );
+
+	$cmb_extra_section_2->add_field( array(
+		'name'       => esc_html__( 'Título', 'cmb2' ),
+		'id'         => $prefix . 'title',
+		'type'       => 'text',
+		'sanitization_cb' => 'prx_allow_html',
+	) );
+
+	$cmb_extra_section_2->add_field( array(
+		'name'       => esc_html__( 'Texto', 'cmb2' ),
+		'id'         => $prefix . 'text',
+		'type'       => 'textarea',
+		'sanitization_cb' => 'prx_allow_html',
+	) );
+
+	$cmb_extra_section_2->add_field( array(
+		'name'       => esc_html__( 'Vídeo do Youtube', 'cmb2' ),
+		'id'         => $prefix . 'youtube',
+		'type'       => 'text',
+		// 'sanitization_cb' => 'prx_allow_html',
+	) );
+
+	$cmb_extra_section_2->add_field( array(
+		'name' 				=> __( 'Imagem de Fundo', 'laf' ),
+		'id'   				=> $prefix . 'bg',
+		'type' 				=> 'file',
+		'text'				=> array(
+			'add_upload_file_text' => __('Adicionar arquivo', 'laf') // Change upload button text. Default: "Add or Upload File"
+		),
+		'attributes' 		=> array(
+			'placeholder' 	=> 'http://',
+			// 'required'		=> ''
+		)
+	) );
+
+	$cmb_extra_section_2->add_field( array(
+		'name' 				=> __( 'Thumb do Vídeo', 'laf' ),
+		'id'   				=> $prefix . 'thumb',
+		'type' 				=> 'file',
+		'text'				=> array(
+			'add_upload_file_text' => __('Adicionar arquivo', 'laf') // Change upload button text. Default: "Add or Upload File"
+		),
+		'attributes' 		=> array(
+			'placeholder' 	=> 'http://',
+			// 'required'		=> ''
+		)
+	) );
+
 	$prefix = 'front_page_section_4_';
 
 	/**
@@ -382,7 +548,7 @@ function front_page_register_metabox() {
 	$group_field_id = $cmb_section_4_tabs->add_field( array(
 		'id'          => $prefix . 'slide',
 		'type'        => 'group',
-		'description' => esc_html__( 'Adicione os slides do banner da Seção #.', 'cmb2' ),
+		'description' => esc_html__( 'Adicione os slides do banner da Seção #4.', 'cmb2' ),
 		'options'     => array(
 			'group_title'   => esc_html__( 'Slide {#}', 'cmb2' ), // {#} gets replaced by row number
 			'add_button'    => esc_html__( 'Adicionar', 'cmb2' ),
